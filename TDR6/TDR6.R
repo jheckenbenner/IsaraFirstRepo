1bdec15f16454f2232b344fbf07cc4d894a0081b
jheckenbenner <jheckenbenner@gmail.com>
2022-05-06 12:20:07 +0200

    ajout elements

--- TDR6/TDR6.R
+++ TDR6/TDR6.R
@@ -93,70 +93,97 @@ head(t3var)
 
 ### Exercice iris1
 
-iris1 <- read.table("data/DesIris.txt", header=TRUE, dec=".", sep=" ")
-library(dplyr)
+
 
 head(iris1)
 filter(iris1, Species=="virginica")
 filter(iris1, SepalLength > 7)
 filter(iris1, SepalLength < 6 & Species=="virginica")
 
-# Sélection dans un vecteur
-iris1$SepalLength[iris1$Species=="virginica"] #Sélection des longueurs de sépales pour les plantes de l'espèce Virginica
-iris1[iris1$Species=="virginica", ]$SepalLength
 
-
-sepales_virginica <- filter(iris1, Species=="virginica")
-select(iris1, SepalLength, Species)
+iris1 %>% 
+  filter(Species=="virginica") %>%
+  select(SepalLength)
 
 
 ### Exercice t3var
 
+# Importer les données dans un objet t3var.
 
-t3var <- read.table("data/t3var.txt", header = TRUE, sep = "\t") #Import des données dans l'objet t3var
-t3var #Affichage de l'ensemble des données 
+t3var <- read.table("data/t3var.txt", header=TRUE, dec=".", sep="\t")
+library(dplyr)
+
+# Identifier les noms des variables de ce jeu de données.
 
 head(t3var)
-names(t3var) #On identifie le nom des variables : "sexe", "poi", "tai" 
+names(t3var)
 
+# Définir le contexte statistique (combien d’individus, de variables, types des variables).
+
+dim(t3var)
+ncol(t3var)
+nrow(t3var)
 
 # Sélectionner les individus 1, 10 et 20.
+
 slice(t3var, 1, 10, 20)
 
+t3var %>% 
+  slice(1, 10, 20)
 
 # Sélectionner les femmes de plus de 170 cm. Combien sont-elles ?
+filter(t3var, sexe=="f" & tai>170)
 
-filter(t3var, sexe=="f" & tai > 170)
-fem170
-nrow(fem170) # Nb de femmes de plus de 170 cm
+t3var %>% 
+  filter(sexe=="f") %>% 
+  filter(tai > 170) %>% 
+  count()
 
-# Variables pour les individus 1 à 20 (sauf la première)
-t3var[10:20, -1]
+# Pour les individus 10 à 20, donner toutes les variables sauf la première.
+t3var %>% 
+  slice(10:20) %>% 
+  select(!sexe) %>% 
+  rename_with(toupper)
+
+# Sélectionner les femmes de taille supérieure à la taille moyenne des femmes, donner l’effectif de ce sous-groupe.
+t3var %>% 
+  filter(sexe=="f") %>% 
+  filter(tai > mean(tai)) %>% 
+  count()
+
+# Donner la moyenne des poids pour tous, puis par sexe.
+t3var %>% 
+  summarise(moy=mean(poi))
+
+t3var %>%
+  select(!sexe) %>% 
+  summarise_all(mean)
+
+t3var %>%
+  group_by(sexe) %>% 
+  summarise_all(mean)
+
+# Donner la variance des poids pour tous, 
+# puis par sexe (la variance estimée ou de l’échantillon).
+t3var %>%
+  group_by(sexe) %>% 
+  summarise_all(sd)
+
+# IMC
+t3var %>% 
+  mutate(taicm=tai/100) %>% 
+  mutate(imc=poi/taicm^2) %>% 
+  arrange(imc)
+
+ 
 
 
-tai <- t3var$tai
-sexe <- t3var$sexe
-taifmoy <- mean(tai[sexe=="f"])#Taille moyenne des femmes 
-taifmoy
 
-# Sélection des femmes de taille supérieure à la moyenne
-fsupmoy <- t3var[tai>taifmoy & sexe=="f", ]
-fsupmoy
-nrow(fsupmoy) # Effectif de femme dont la taille est sup. à la moyenne : 12
 
-# Moyenne des poids pour tous
-poi <- t3var$poi
-mean(poi)
 
-# Moyenne des poids par sexe
-mean(poi[sexe=="f"])
-mean(poi[sexe=="h"])
 
-# Variance des poids pour tous
-var(poi)
 
-# Variance par sexe 
-var(poi[sexe=="f"])
-var(poi[sexe=="h"])
+
+
 
 
